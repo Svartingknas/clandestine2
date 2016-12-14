a4db0cd480f50a5e4337c7b2912be88203b8c6da
Svartingknas <svartingknas@gmail.com>
2016-12-14 16:11:05 -0700

    heroku test add modal contact us

---
+++ src/Contact.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+export default class Contact extends React.Component{
+  render(){
+    return(
+      <div className="container">
+        <h2>Basic Modal Example</h2>
+        {/*Trigger the modal with a button*/}
+        <button type="button" className="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+        {/*Modal*/}
+        <div className="modal fade" id="myModal" role="dialog">
+          <div className="modal-dialog">
+
+        {/*Modal content*/}
+      <div className="modal-content">
+        <div className="modal-header">
+          <button type="button" className="close" data-dismiss="modal">&times;</button>
+          <h4 className="modal-title">Modal Header</h4>
+        </div>
+        <div className="modal-body">
+          <form className="form-horizontal" method="post" role="form">
+    	<div className="form-group">
+    		<label htmlFor="email" className="col-sm-2 control-label">E-mail</label>
+    		<div className="col-sm-4">
+    			<input type="email" className="form-control" id="email" name="email" placeholder="E-mail Address" required autoFocus />
+    		</div>
+    	</div>
+
+    	<div className="form-group">
+    		<label htmlFor="name" className="col-sm-2 control-label">Name</label>
+    		<div className="col-sm-4">
+    			<input type="text" className="form-control" id="name" name="name" placeholder="Name" required />
+    		</div>
+    	</div>
+
+    	<div className="form-group">
+    		<label htmlFor="name" className="col-sm-2 control-label">Message</label>
+    		<div className="col-sm-6">
+    			<textarea id="message" name="message" className="form-control" placeholder="Your Message" rows="5" required />
+    		</div>
+    	</div>
+
+    	<div className="form-group">
+    		<div className="col-sm-offset-2 col-sm-2">
+    			<button type="submit" className="btn btn-primary">Send Message</button>
+    		</div>
+    	</div>
+    </form>
+        </div>
+        <div className="modal-footer">
+          <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+</div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import index from './index.css'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
-// import Events from './Events'
-import About from './About'
-import Repo from './Repo'
 import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+// import Events from './Events'
 // import Past from './Past'
-// import Contact from './Contact'
+import Repo from './Repo'
 
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={App} >
       <IndexRoute component={Home} />
       <Route path="/about" component={About} >
-        {/*<Route path="/about/:userName/:repoName" component={Contact}/>*/}
+        <Route path="/about/:userName/:repoName" component={Contact}/>
       </Route>
       {/*<Route path="/events" component={Events} >
         <Route path="/events/:userName/:repoName" component={Past} />
