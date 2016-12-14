256127d1b0e68b964ba28cafade818fc8a112de5
Svartingknas <svartingknas@gmail.com>
2016-12-14 14:06:06 -0700

    heroku test header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header'
+// import Sidebar from './Sidebar'
+// import Footer from './Footer'
+// import Home from './Home'
 
-class App extends Component {
-  render() {
+
+
+
+export default class App extends React.Component {
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        /*<Header />
+          <hr />
+          <div>
+            <div className='col-sm-9'>
+
+            </div>
+              {/*<Sidebar />
+              <hr />
+              <Footer />*/}
+            </div>
       </div>
+
     );
   }
 }
-
-export default App;
