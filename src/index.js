4bbc5f0565f7268895451259328454dd2f772b40
Svartingknas <svartingknas@gmail.com>
2016-12-14 15:56:53 -0700

    heroku test router index about

--- src/index.js
+++ src/index.js
@@ -2,8 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import index from './index.css'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+// import Events from './Events'
+import About from './About'
+import Repo from './Repo'
+import Home from './Home'
+// import Past from './Past'
+// import Contact from './Contact'
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App} >
+      <IndexRoute component={Home} />
+      <Route path="/about" component={About} >
+        {/*<Route path="/about/:userName/:repoName" component={Contact}/>*/}
+      </Route>
+      {/*<Route path="/events" component={Events} >
+        <Route path="/events/:userName/:repoName" component={Past} />
+      </Route>*/}
+    </Route>
+</Router>,
   document.getElementById('root')
 );
