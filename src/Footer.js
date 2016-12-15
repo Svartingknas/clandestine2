0fc64cff17900f1d3fd1e59bebcb04ff42a81e6d
Svartingknas <svartingknas@gmail.com>
2016-12-15 01:26:59 -0700

    feat(style)modal adjustment

--- src/Footer.js
+++ src/Footer.js
@@ -9,63 +9,60 @@ export default class Footer extends React.Component {
       <div>
         <hr />
         <footer>
+          <div className="container-fluid">
+            <Link className="glyphicon glyphicon-home" to="/"></Link>&nbsp;
+            {/*Trigger the modal with a button*/}
+            <Link type="button" className="glyphicon glyphicon-envelope" data-toggle="modal" data-target="#myModal"></Link>
 
-            <div className="container-fluid">
-              <Link className="glyphicon glyphicon-home" to="/"></Link>&nbsp;
-              {/*Trigger the modal with a button*/}
-              <Link type="button" className="glyphicon glyphicon-envelope" data-toggle="modal" data-target="#myModal"></Link>
+            {/*Modal*/}
+            <div className="modal fade" id="myModal" role="dialog">
+              <div className="modal-dialog">
 
-              {/*Modal*/}
-              <div className="modal fade" id="myModal" role="dialog">
-                <div className="modal-dialog">
-
-                {/*Modal content*/}
-            <div className="modal-content">
-              <div className="modal-header">
-                <button type="button" className="close" data-dismiss="modal">&times;</button>
-                <h4 className="modal-title">Contact Us Via E-mail</h4>
-              </div>
-              <div className="modal-body">
-                <form className="form-horizontal" method="post" role="form">
-            <div className="form-group">
-              <label htmlFor="email" className="col-sm-2 control-label">E-mail</label>
-              <div className="col-sm-4">
-                <input type="email" className="form-control" id="email" name="email" placeholder="E-mail Address" required autoFocus />
-              </div>
+              {/*Modal content*/}
+          <div className="modal-content">
+            <div className="modal-header">
+              <button type="button" className="close" data-dismiss="modal">&times;</button>
+              <h4 className="modal-title">E-mail us here</h4>
             </div>
+            <div className="modal-body">
+              <form className="form-horizontal" method="post" role="form">
+        	<div className="form-group">
+        		<label htmlFor="email" className="col-sm-2 control-label">E-mail</label>
+        		<div className="col-sm-4">
+        			<input type="email" className="form-control" id="email" name="email" placeholder="E-mail Address" required autoFocus />
+        		</div>
+        	</div>
 
-            <div className="form-group">
-              <label htmlFor="name" className="col-sm-2 control-label">Name</label>
-              <div className="col-sm-4">
-                <input type="text" className="form-control" id="name" name="name" placeholder="Name" required />
-              </div>
-            </div>
+        	<div className="form-group">
+        		<label htmlFor="name" className="col-sm-2 control-label">Name</label>
+        		<div className="col-sm-4">
+        			<input type="text" className="form-control" id="name" name="name" placeholder="Name" required />
+        		</div>
+        	</div>
 
-            <div className="form-group">
-              <label htmlFor="name" className="col-sm-2 control-label">Message</label>
-              <div className="col-sm-6">
-                <textarea id="message" name="message" className="form-control" placeholder="Your Message" rows="5" required />
-              </div>
-            </div>
+        	<div className="form-group">
+        		<label htmlFor="name" className="col-sm-2 control-label">Message</label>
+        		<div className="col-sm-6">
+        			<textarea id="message" name="message" className="form-control" placeholder="Your Message" rows="5" required />
+        		</div>
+        	</div>
 
-            <div className="form-group">
-              <div className="col-sm-offset-2 col-sm-2">
-                <button type="submit" className="btn btn-primary">Send Message</button>
-              </div>
+        	<div className="form-group">
+        		<div className="col-sm-offset-2 col-sm-2">
+        			<button type="submit" className="btn btn-primary">Send Message</button>
+        		</div>
+        	</div>
+        </form>
             </div>
-          </form>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-              </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
             </div>
-
           </div>
+
         </div>
-
       </div>
-          <Link ></Link>	&nbsp;
 
+    </div>
           <div><div className="fb-like" data-href="https://clandestineslc.com/" data-layout="button" data-action="recommend" data-size="small" data-show-faces="false" data-share="true"></div></div>
         </footer>
           <hr />
